fc78d8a454660fd3a9c1480a23a3ecf44fc4f7e1
Rahuldotecon <128730689+Rahuldotecon@users.noreply.github.com>
2023-03-23 22:55:54 +0530

    Add files via upload

---
+++ my assignment jupyter.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# MY JUPITER NOTEBOOK 
+
+# **RAHUL**
+# 
+# STUDENT OF ECONOMICS 
+
+# *I want to do Data Science so that I can find insights regarding from data generated from population ,so that I can help my country Economy in public welfare and to get pareto efficient allocation of different resourses*
+
+# Data sciene language : ***python,R,Java,SQL,JULIA,C++,SCALA,JAVA SCRIPT,etc.***
+
+# **Data science libraries** :
+# TensorFlow
+# NumPy
+# SciPy 
+# Pandas
+# Matplotlib 
+# Keras
+# SciKit-Learn
+# PyTorch
+# Scrapy
+
+# ### my code gonna find average marks scored by a school class in Economics ### 
+
+# In[3]:
+
+
+numbers=[45,56,53,86,89,57,35,99,75,68,52,45,56,77,88,65]
+average=sum(numbers)/len(numbers)
+print("average of list:",round(average,3))
+
+
+# What I learn from this course
+# 
+#  - *how to create juypter notebook*   Example of bullet with bold,italic format
+#  - *how to write codes*
+#  - **markdown and how to run codes**
+#  website for coursera to find out useful course https://www.simplilearn.com/tutorials/python-tutorial/find-average-of-list-in-python#:~:text=To%20find%20the%20average%20of%20the%20numbers%20in%20a%20list,function%20from%20the%20statistics%20module.
+#  
+#  ~I've write these all codes and markdown myself~  sample of strikeout
+
+# In[8]:
+
+
+x=1055
+y=1456
+z=1452
+m=7549
+print(x+y+z+m)
+
+
+# In[ ]:
+
+
+
+
